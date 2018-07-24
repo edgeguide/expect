3b934ab20b7c438a19b7fdf187b2c933825c3c32
Marwan Khalili <marwan.khalili@edgeguide.com>
2018-07-24 17:17:18 +0200

    Added test cases for any type

--- tests/types/any.test.js
+++ tests/types/any.test.js
@@ -17,6 +17,18 @@ describe('Expect package (any validation):', () => {
     });
   });
 
+  it('respects allowNull', () => {
+    const expectModule = require('../../src');
+    const tests = [null, undefined, ''];
+    tests.forEach(test => {
+      const expectations = expectModule(
+        { test: { type: 'any', allowNull: true } },
+        { test }
+      );
+      expect(expectations.wereMet()).toBe(true);
+    });
+  });
+
   it('parse function can be used with any', () => {
     const expectModule = require('../../src');
     const expectations = expectModule(
@@ -24,5 +36,67 @@ describe('Expect package (any validation):', () => {
       { test: 123 }
     );
     expect(expectations.getParsed()).toEqual({ test: '123' });
+  });
+
+  it('allowNull has higher priority than parse', () => {
+    const expectModule = require('../../src');
+    const expectations = expectModule(
+      { test: { type: 'any', allowNull: false, parse: () => 'test' } },
+      { test: null }
+    );
+
+    expect(expectations.wereMet()).toBe(false);
+  });
+
+  it('respects requiredIf', () => {
+    const expectModule = require('../../src');
+
+    const validExpectations = expectModule(
+      {
+        test: { type: 'any', requiredIf: 'foo' },
+        foo: { type: 'string', allowNull: true }
+      },
+      {}
+    );
+
+    const invalidExpectations = expectModule(
+      {
+        test: { type: 'any', requiredIf: 'foo' },
+        foo: { type: 'string', allowNull: true }
+      },
+      { foo: '123' }
+    );
+
+    expect(validExpectations.wereMet()).toBe(true);
+    expect(invalidExpectations.wereMet()).toBe(false);
+  });
+
+  it('respects errorCode', () => {
+    const expectModule = require('../../src');
+    const expectations = expectModule(
+      { test: { type: 'any', errorCode: 'error' } },
+      {}
+    );
+
+    expect(expectations.errors()).toEqual({ test: ['error'] });
+  });
+
+  it('respects nullCode', () => {
+    const expectModule = require('../../src');
+    const expectations = expectModule(
+      { test: { type: 'any', nullCode: 'missing' } },
+      {}
+    );
+
+    expect(expectations.errors()).toEqual({ test: ['missing'] });
+  });
+
+  it('nullCode has higher priorty than errorCode', () => {
+    const expectModule = require('../../src');
+    const expectations = expectModule(
+      { test: { type: 'any', nullCode: 'missing', errorCode: 'error' } },
+      {}
+    );
+    expect(expectations.errors()).toEqual({ test: ['missing'] });
   });
 });
